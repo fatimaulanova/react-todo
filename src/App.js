3d10254bc254d64890eda9dded20d02aceba8583
fatimaulanova <kanalbekovafatima@gmail.com>
2023-01-03 23:23:12 +0600

    initial

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, {useState} from 'react'
+import AddTaskForm from './components/AddTaskForm/AddTaskForm'
+import Todo from './components/toDo/ToDo'
+import UpdateForm from './components/UpdateForm/UpdateForm'
+import './App.css';
+
+function App(){
+    // Task State
+    const [toDo, setToDo] = useState([
+        {'id': 1, 'title': 'Task 1', 'status': false},
+        {'id': 2, 'title': 'Task 2', 'status': false}
+    ])
+    // Temp State
+    const [newTask, setNewTask] = useState('')
+    const [updateData, setUpdateData] = useState('')
+    // Add task
+    const addTask = () => {
+        if(newTask){
+            let num = toDo.length + 1
+            let newEntry = { id: num, title: newTask, status: false }
+            setToDo([...toDo, newEntry])
+            setNewTask('')
+        }
+    }
+    // delete Task
+    const deleteTask = (id) => {
+        let newTasks = toDo.filter(task => task.id !==id)
+        setToDo(newTasks)
+    }
+    // mark task as done or completed
+    const markDone = (id) => {
+        let newTask = toDo.map(task => {
+            if (task.id === id){
+                return({...task, status: !task.status})
+            }
+            return task
+        })
+        setToDo(newTask)
+    }
+    // cancel update
+    const cancelUpdate = () => {
+        setUpdateData('')
+    }
+    // change task for update
+    const changeTask = (e) => {
+       let newEntry = {
+           id: updateData.id,
+           title: e.target.value,
+           status: updateData.status ? true : false
+       }
+       setUpdateData(newEntry)
+    }
+    // update task
+    const updateTask = () => {
+        let filterRecords = [...toDo].filter( task => task.id !== updateData.id )
+        let updatedObject = [...filterRecords, updateData]
+        setToDo(updatedObject)
+        setUpdateData('')
+    }
+  return (
+    <div className="container App">
+      <h1>To Do List App (React Js)</h1>
+        {/*update task*/}
+        { updateData && updateData ? (
+          <UpdateForm
+              updateData={updateData}
+              changeTask={changeTask}
+              updateTask={updateTask}
+              cancelUpdate={cancelUpdate}
+          />
+        ) : (
+           <AddTaskForm
+               newTask={newTask}
+               setNewTask={setNewTask}
+               addTask={addTask}
+           />
+        ) }
+        {/* Display ToDos*/}
+        {toDo && toDo.length ? '' : 'No Tasks...'}
+        <Todo
+            toDo={toDo}
+            markDone={markDone}
+            setUpdateData={setUpdateData}
+            deleteTask={deleteTask}
+        />
+    </div>
+  );
+}
+
+export default App;
